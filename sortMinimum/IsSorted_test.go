3143c8433e2a46cc6be69d92b08056c3dd47369b
jkuya <kuyajesee@proton.me>
2024-12-09 17:07:38 +0300

    Test: add a test for negative numbers'

--- sortMinimum/IsSorted_test.go
+++ sortMinimum/IsSorted_test.go
@@ -10,6 +10,7 @@ var TestCases = []struct {
 	{"Test1", []int{1, 2, 3, 4, 5}, true},
 	{"Test2", []int{5, 4, 1, 9, 2}, false},
 	{"Test3", []int{8, 99, 100, 103, 600}, true},
+	{"Test4", []int{-20, -17, -11, -5, -2, 1}, true},
 }
 
 func TestCheckStack(t *testing.T) {
