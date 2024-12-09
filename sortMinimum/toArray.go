16f3944be9e014acfe5dee145167e26d60d2979a
jkuya <kuyajesee@proton.me>
2024-12-09 17:32:13 +0300

    Test: hundle a test for trimming ,

--- sortMinimum/toArray.go
+++ sortMinimum/toArray.go
@@ -9,6 +9,7 @@ func ToArray(str string) ([]int, string) {
 	arr := strings.Split(str, " ")
 	var res []int
 	for i := 0; i < len(arr); i++ {
+		arr[i] = strings.Trim(arr[i], ",")
 		num, err := strconv.Atoi(arr[i])
 		if err != nil {
 			return nil, "not a number"
