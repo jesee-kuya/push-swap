93534a0e933961d071e3893ab9d030e881b92571
jkuya <kuyajesee@proton.me>
2024-12-19 16:06:58 +0300

    Test: add a test for checker

---
+++ sortMinimum/checker_test.go
@@ -0,0 +1,25 @@
+package sortMinimum
+
+import "testing"
+
+var TestCases18 = []struct {
+	name     string
+	arr      []int
+	ops      []string
+	expected string
+}{
+	{"Test1", []int{2, 1, 3, 6, 5, 8}, []string{"sa", "pb", "pb", "pb", "sa", "pa", "pa", "pa"}, "OK"},
+	{"Test2", []int{0, 9, 1, 8, 2}, []string{"pb", "ra", "pb", "ra", "sa", "ra", "pa", "pa"}, "OK"},
+}
+
+func TestChecker(t *testing.T) {
+	for _, tc := range TestCases18 {
+		t.Run(tc.name, func(t *testing.T) {
+			got := Checker(tc.arr, tc.ops)
+
+			if got != tc.expected {
+				t.Errorf("Expected %v but got %v", tc.expected, got)
+			}
+		})
+	}
+}
