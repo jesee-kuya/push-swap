2bfeae54e3eedf4eb98d486a333cb73463deca10
jkuya <kuyajesee@proton.me>
2024-12-19 13:42:35 +0300

    Feat: handle more than 10 numbers

--- sortMinimum/pushminimum.go
+++ sortMinimum/pushminimum.go
@@ -6,13 +6,25 @@ import "fmt"
 func PushMinimum(input []int) []string {
 	operations := []string{}
 	arr := append([]int{}, input...)
+	count := 0
 
-	for !IsSorted(arr) {
-		ops, updatedArr, tempStack := PushSwap(arr)
-		operations = append(operations, ops...)
-		resl, finalArr := ProcessTempStack(updatedArr, tempStack)
-		operations = append(operations, resl...)
-		arr = finalArr
+	if len(arr) > 9 {
+		for !IsSorted(arr) {
+			ops, updatedArr, tempStack := PreEmpt(arr)
+			operations = append(operations, ops...)
+			resl, finalArr := ProcessTempStack(updatedArr, tempStack)
+			operations = append(operations, resl...)
+			arr = finalArr
+			count++
+		}
+	} else {
+		for !IsSorted(arr) {
+			ops, updatedArr, tempStack := PushSwap(arr)
+			operations = append(operations, ops...)
+			resl, finalArr := ProcessTempStack(updatedArr, tempStack)
+			operations = append(operations, resl...)
+			arr = finalArr
+		}
 	}
 
 	operations = SortOperations(operations)
