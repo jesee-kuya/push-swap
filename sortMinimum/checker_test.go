5d27e8d8ed7fee6c586c8c3c247355d5885beb2a
jkuya <kuyajesee@proton.me>
2025-01-10 15:51:58 +0300

    Feat: add a test

--- sortMinimum/checker_test.go
+++ sortMinimum/checker_test.go
@@ -10,6 +10,8 @@ var TestCases18 = []struct {
 }{
 	{"Test1", []int{2, 1, 3, 6, 5, 8}, []string{"sa", "pb", "pb", "pb", "sa", "pa", "pa", "pa"}, "OK"},
 	{"Test2", []int{0, 9, 1, 8, 2}, []string{"pb", "ra", "pb", "ra", "sa", "ra", "pa", "pa"}, "OK"},
+	{"Test3", []int{924, 169, 992, 451, 685}, []string{"sa", "pb", "rra", "rra", "pa"}, "OK"},
+	{"Test4", []int{9, 10, 1, 4, 15}, []string{"sa", "sa", "pb", "pb", "sa"}, "KO"},
 }
 
 func TestChecker(t *testing.T) {
